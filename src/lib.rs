cac3fd6cb251fabc85a758df42f9dfc5953facc5
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2022-06-12 17:13:43 -0500

    change contract
    changed contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 #![allow(non_snake_case)]
-use near_sdk::serde_json::Map;
 use near_sdk::{BorshStorageKey, near_bindgen, PanicOnDefault, AccountId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::string;
+use std::i8;
  
 #[derive(BorshStorageKey, BorshSerialize)]
 enum Position {
@@ -45,11 +44,18 @@ genesisStartOnce : bool,
  
 adminAddress : i8, // address of the admin
 operatorAddress : i8, // address of the operator
+_adminAddress : i8, // address of the admin
+_operatorAddress : i8, // address of the operator
  
 bufferSeconds : u128, // number of seconds for valid execution of a prediction round
 intervalSeconds : u128, // interval in seconds between two prediction rounds
+intervalBlocks : u128, // interval in seconds between two prediction rounds
+bufferBlocks : u128, // number of seconds for valid execution of a prediction round
+_bufferBlocks : u128, // number of seconds for valid execution of a prediction round
+_intervalBlocks : u128, // interval in seconds between two prediction rounds
  
 minBetAmount : u128, // minimum betting amount (denominated in wei)
+_minBetAmount : u128, // minimum betting amount (denominated in wei)
 treasuryFee : u128, // treasury rate (e.g. 200 = 2%, 150 = 1.50%)
 treasuryAmount : u128, // treasury amount that was not claimed
  
@@ -57,6 +63,7 @@ currentEpoch : u128, // current epoch for prediction round
  
 oracleLatestRoundId : u128, // converted from uint80 (Chainlink)
 oracleUpdateAllowance : u128, // seconds
+_oracleUpdateAllowance : u128, // seconds
  
 MAX_TREASURY_FEE : u128, // 10%
  
@@ -65,6 +72,35 @@ userRounds: HashMap<String ,u128>,
 ledger: HashMap<String, u128>,
  
 }
+
+fn setAdmin(_adminAddress: i8, adminAddress: i8 ){
+  _adminAddress > 0;
+  adminAddress == _adminAddress;
+}
+
+fn setOperator(_operatorAddress: i8, operatorAddress: i8){
+  _operatorAddress > 0;
+  operatorAddress == _operatorAddress;
+}
+
+fn setIntervalBlocks(_intervalBlocks: u128, intervalBlocks: u128){
+  intervalBlocks == _intervalBlocks;
+}
+
+fn setBufferBlocks(_bufferBlocks: u128, intervalBlocks: u128, bufferBlocks: u128){
+  _bufferBlocks <= intervalBlocks;
+  bufferBlocks == _bufferBlocks;
+}
+
+fn setRewardRate(_rewardRate: u128, rewardRate: u128, treasuryRate: u128){
+  _rewardRate <= TOTAL_RATE;
+  rewardRate = _rewardRate;
+  treasuryRate = TOTAL_RATE.sub(_rewardRate);
+
+  emit RatesUpdated(currentEpoch, rewardRate, treasuryRate);
+}
+
+
 #[near_bindgen]
 impl Contract {
 #[init]
@@ -87,6 +123,14 @@ pub fn new() -> Self {
     rounds: HashMap::new(),
     userRounds: HashMap::new(),
    
+    _adminAddress : 0,
+    _operatorAddress : 0,
+    _intervalBlocks : 0,
+    _bufferBlocks : 0,
+    _minBetAmount : 0,
+    _oracleUpdateAllowance : 0,
+    intervalBlocks: 0,
+    bufferBlocks: 0,
    
   }
 }
